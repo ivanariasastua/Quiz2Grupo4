4bce29dac5235e59bd9f5fad303478017bfa4d1d
ivanariasastua <69368731+ivanariasastua@users.noreply.github.com>
2020-10-16 18:28:10 -0600

    Correccion del ProductosExistencias

--- facturacion/src/main/java/org/una/tienda/facturacion/dto/ProductosExistenciasDTO.java
+++ facturacion/src/main/java/org/una/tienda/facturacion/dto/ProductosExistenciasDTO.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package org.una.tienda.facturacion.entities;
+package org.una.tienda.facturacion.dto;
 
 /**
  *
  * @author Ivan Josué Arias Astua
  */
-public class ProductosExistentes {
+public class ProductosExistenciasDTO {
     
 }
--- facturacion/src/main/java/org/una/tienda/facturacion/entities/ProductosExistencias.java
+++ facturacion/src/main/java/org/una/tienda/facturacion/entities/ProductosExistencias.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package org.una.tienda.facturacion.dto;
+package org.una.tienda.facturacion.entities;
 
 /**
  *
  * @author Ivan Josué Arias Astua
  */
-public class ProductosExistentesDTO {
+public class ProductosExistencias {
     
 }
