f91610be95b53b3662b50c9ac77c7bf22e0a60ac
ivanariasastua <69368731+ivanariasastua@users.noreply.github.com>
2020-10-16 20:05:53 -0600

    Factura y FacturaDetalle service interfaz sin codigo

---
+++ facturacion/src/main/java/org/una/tienda/facturacion/service/IFacturaService.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.una.tienda.facturacion.service;
+
+/**
+ *
+ * @author Ivan Josué Arias Astua
+ */
+public interface IFacturaService {
+    
+}
